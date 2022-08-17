32fec262da11b9389a59ef42149508f83bb69a7c
Deryn Rushworth <derynrushworth@gmail.com>
2022-08-17 11:51:48 +0100

    added growUp as a method on Pet's prototype

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -2,6 +2,28 @@ const Pet = require('../src/pet');
 
 describe('constructor', () => {
     it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
+      expect(new Pet('Fluffy')).toBeInstanceOf(Object);
+    });
+  });
+
+  describe('constructor', () => {  
+    it('sets the name property', () => {
+      const pet = new Pet('Fluffy');
+      expect(pet.name).toEqual('Fluffy');
+    });
+  });
+
+  describe('constructor', () => {
+    it('has a initial age of 0', () => {
+      const pet = new Pet('Fluffy');
+      expect(pet.age).toEqual(0);
+    });
+  });
+
+  describe('growUp', () => {
+    it('increments the age by 1', () => {
+      const pet = new Pet('Fido');
+      pet.growUp();
+      expect(pet.age).toEqual(1);
     });
   });
